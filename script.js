1f8a92dd834def42c75a8546c9980d351f445356
Nixoonn <100475006+Nixoonn@users.noreply.github.com>
2023-01-27 10:51:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,12 @@
-const tweetForm = document.querySelector("form");
-const tweetInput = document.querySelector("#tweet-input");
-const tweetContainer = document.querySelector("#tweet-container");
+const chatlog = document.getElementById("chatlog");
+const chatform = document.getElementById("chatform");
+const chatnick = document.getElementById("chatnick");
+const chatinput = document.getElementById("chatinput");
 
-tweetForm.addEventListener("submit", function(e) {
-  e.preventDefault();
-  const tweet = tweetInput.value;
-  if (tweet === "") {
-    return;
-  }
-  addTweet(tweet);
-  tweetInput.value = "";
+chatform.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const message = `<p><strong>${chatnick.value}:</strong> ${chatinput.value}</p>`;
+  chatlog.innerHTML += message;
+  chatinput.value = "";
 });
-
-function addTweet(tweet) {
-  const newTweet = document.createElement("div");
-  newTweet.innerHTML = tweet;
-  tweetContainer.appendChild(newTweet);
-}
